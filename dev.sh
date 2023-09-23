f7b16d6d5192d53d20deae806d0a5d3fca26c5a0
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2023-09-23 23:11:09 +0100

    Make app almost runnable with some hacks

--- dev.sh
+++ dev.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-./db.sh
-pnpm run dev
+if [ ! -x "$(command -v mkcert)" ];
+then
+    curl -JLO "https://github.com/FiloSottile/mkcert/releases/download/v1.4.4/mkcert-v1.4.4-linux-amd64"
+    chmod +x mkcert-v*-linux-amd64
+    cp mkcert-v*-linux-amd64 /usr/local/bin/mkcert
+    rm mkcert-v*-linux-amd64
+fi
+if [[ ! -f localhost.cert ]]
+then
+    mkcert -install
+    mkcert -cert-file localhost.cert -key-file localhost.key ni.fe.up.pt
+fi
+
+
+if [ -x "$(command -v podman-compose)" ]; then
+    podman-compose up
+else
+    docker-compose up
+fi
